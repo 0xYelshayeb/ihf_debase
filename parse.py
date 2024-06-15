d6ee62a54fd6d39427d162c74a3ad15f22c4099a
Yassine <ge87won@mytum.de>
2024-06-15 13:39:40 +0200

    updated addresses

---
+++ parse.py
@@ -0,0 +1,19 @@
+import csv
+
+def extract_addresses(file_path):
+    addresses = []
+
+    with open(file_path, mode='r', newline='') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            addresses.append(row['HolderAddress'])
+
+    return addresses
+
+file_path = 'holders.csv'
+
+# Extract addresses
+addresses = extract_addresses(file_path)
+
+# Print the list of addresses
+print(addresses[:100])
