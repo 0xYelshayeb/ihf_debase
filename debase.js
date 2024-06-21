90afa6492b627a69ab01720a755d5ba70d9a848d
Yassine <ge87won@mytum.de>
2024-06-21 13:03:02 +0200

    Pushed with git fast

--- debase.js
+++ debase.js
@@ -89,7 +89,7 @@ const addresses = [
     '0xf6789053ecd92fa29dbd3dd8ee553350192035f4', '0x19e8f0f4a73babfee77450acbffe683cb6eb8825', '0x45c7509a5e9bc8c839931f56f097dbc3ce5e1a6e', '0x961da3f96b27eb356239589bb022412168133ed2', '0x8e398e3d8b6d9ff90d54f86ff98a1d271af4c0ff', '0x8f167478610984ef4a69780c6b15eec4b8d2c7f7', '0x2e7cd7b71f5f6efb78f2154f115f7cded8b70e73', '0x80ebda07a4aa13eb6de7ca95dfd923e7eeb5c41d', '0xeb1e6e1aa73b1f5c2c4448461e6f1e29c8c92966', '0xa87dccdc8f445aabc1e7e179e573302da905b1a8'
 ];
 
-const THRESHOLD = ethers.utils.parseEther('0.000005');
+const THRESHOLD = ethers.utils.parseEther('0.0000008');
 
 const getTimeStamp = () => {
     const now = new Date();
